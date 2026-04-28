d21252757d1ddada18d67182649d4d29f96a35bc
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-28 17:35:42 -0600

    contructors

--- src/Aritmetica.java
+++ src/Aritmetica.java
@@ -2,6 +2,18 @@ public class Aritmetica {
     int operando1;
     int operando2;
 
+    //constructores
+    //constructor vacio
+    public Aritmetica(){}
+
+    //sobrecarga de constructores
+    public Aritmetica(int opt1, int opt2){
+        System.out.println("Ejecutando constructor");
+
+        operando1 = opt1;
+        operando2 = opt2;
+    }
+
     public void sumar() {
         System.out.println("Resultado de la suma: " + (operando1 + operando2));
     }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ void main() {
     //Cabe a destacar que tecnicamente estoy reaalizando todos estos ejercios como practica
     //o como reeforzamiento de lo que he aprrendido mucho tiempo atras, ademas de ver las nuevas sintaxis
 
+    //Por cierto el curso en este momento esta apenas mostrando los constructores para poder acceder
+    //desde otras clases asi que ese cambio no se mirara en este repositorio
+
     System.out.println("*** Creacion de clases y objetos ***");
     Persona luis = new Persona();
 
@@ -22,12 +25,12 @@ void main() {
     juan.email = "juan.gutierrez@gmail.com";
     juan.celular = "54821485";
     juan.mostrarPersona();
+    System.out.println();
 
-    Aritmetica aritmetica1 = new Aritmetica();
+    Aritmetica aritmetica1 = new Aritmetica();//al crear esto se ejecuta el constructor
 
     aritmetica1.operando1 = 5;
     aritmetica1.operando2 = 7;
-
     aritmetica1.sumar();
     aritmetica1.restar();
 }
