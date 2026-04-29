e420a615a8084849011d5b257824b83e9ad9f41d
Luisorozco42 <lorozcolazo@gmail.com>
2026-04-29 00:45:05 -0600

    static atributes

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import aritmetica.Aritmetica;
+import com.sun.nio.sctp.PeerAddressChangeNotification;
 import herencia.Animal;
 import persona.Persona;
 
@@ -42,4 +43,12 @@ void main(String[] args) {
 
     //Animal animal1 = new Animal();
     //animal1.dormir(); no se puede acceder por la palabra reservada protected
+    System.out.println("Variable estatica: " + Persona.getContadorPersona());//recordemos que para poder usarlos fuera del paquete es necesario tenerlo en public
+    var objeto1 = new Persona("Luis", "Orozco", "lorozcolazo@gmail.com", "51544156");
+    System.out.println(objeto1);//comportamiento por default es el toString()
+    System.out.println("Variable estatica: " + Persona.getContadorPersona());
+    //segundo objeto
+    var objeto2 = new Persona("Ian", "Gomez","ian.gomez@gmail.com","56178528");
+    System.out.println(objeto2);
+    System.out.println("Variable estatica: " + Persona.getContadorPersona());// Es posble acceder desde objeeto.contadorPersona pero la buena practica es acceder desde la clase en si
 }
--- src/persona/Persona.java
+++ src/persona/Persona.java
@@ -3,23 +3,26 @@ package persona;
 public class Persona {
     // originalmente, era práctica hora se volvió el reto #22
     //bueno luego tocaba hacer otra clase dentro del mismo paquete, pero es lo mismo a que lo haga en main
+    private static int contadorPersonas = 0; //recordemos que los static no funcionan como los demás atributos
+    private int idPersona;
     private String nombre;
     private String apellido;
     private String email;
     private String celular;
 
-    public Persona() {}
+    public Persona() {this.idPersona = ++Persona.contadorPersonas;}
 
     public Persona(String nombre, String apellido, String email, String celular) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.email = email;
         this.celular = celular;
+        this.idPersona = ++Persona.contadorPersonas;//asignamos el idUnico con la variable estatica
     }
 
-    public String getNombre() {
-        return this.nombre;
-    }
+    public int getIdPersona() {return idPersona;}
+
+    public String getNombre() {return this.nombre;}
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
@@ -49,10 +52,9 @@ public class Persona {
         this.celular = celular;
     }
 
-    public void mostrarPersona(){
-        System.out.println("nombre = " + nombre);
-        System.out.println("apellido = " + apellido);
-        System.out.println("email = " + email);
-        System.out.println("celular = " + celular);
-    }
+    public static int getContadorPersona(){return Persona.contadorPersonas;}
+
+    //demostrando por qué todas las clases son objetos
+    @Override
+    public String toString(){return "Id: " + this.idPersona + ", Nombre: " + this.nombre + ", Apellido: " + this.apellido;}
 }
